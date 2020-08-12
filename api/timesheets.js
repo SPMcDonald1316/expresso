1be53c5b5944ada5e6a0efab8d4a985bb4aeb8bf
Sean McDonald <spmcdonald1316@gmail.com>
2020-08-12 16:02:34 -0500

    code post handler for timesheets

--- api/timesheets.js
+++ api/timesheets.js
@@ -17,4 +17,29 @@ timesheetsRouter.get('/', (req, res, next) => {
   });
 });
 
+timesheetsRouter.post('/', (req, res, next) => {
+  const timesheet = req.body.timesheet;
+  if (!timesheet.hours || !timesheet.rate || !timesheet.date) {
+    return res.sendStatus(400);
+  }
+  db.run('INSERT INTO Timesheet (hours, rate, date, employee_id) VALUES ($hours, $rate, $date, $employeeId)', {
+    $hours: timesheet.hours,
+    $rate: timesheet.rate,
+    $date: timesheet.date,
+    $employeeId: req.params.employeeId
+  }, function(err) {
+    if (err) {
+      next(err);
+    } else {
+      db.get(`SELECT * FROM Timesheet WHERE id = ${this.lastID}`, (err, timesheet) => {
+        if (err) {
+          next(err);
+        } else {
+          res.status(201).json({timesheet: timesheet});
+        }
+      });
+    }
+  });
+});
+
 module.exports = timesheetsRouter;

c77dc31adc986f5394241d6a53b816c693658b08
Sean McDonald <spmcdonald1316@gmail.com>
2020-08-12 16:08:39 -0500

    add route parameter for timesheet

--- api/timesheets.js
+++ api/timesheets.js
@@ -6,6 +6,19 @@ const timesheetsRouter = express.Router({mergeParams: true});
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+//Router parameter
+timesheetsRouter.param('timesheetId', (req, res, next, id) => {
+  db.get(`SELECT * FROM Timesheet WHERE id = ${id}`, (err, timesheet) => {
+    if (err) {
+      next(err);
+    } else if (timesheet) {
+      next();
+    } else {
+      res.sendStatus(404);
+    }
+  });
+});
+
 //Routes
 timesheetsRouter.get('/', (req, res, next) => {
   db.all(`SELECT * FROM Timesheet WHERE employee_id = ${req.params.employeeId}`, (err, timesheets) => {

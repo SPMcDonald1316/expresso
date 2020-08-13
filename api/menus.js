5c6e1ea323b3dd7dff72828e0427f9383c0a6261
Sean McDonald <spmcdonald1316@gmail.com>
2020-08-13 14:11:33 -0500

    code get all and post handlers for menus

--- api/menus.js
+++ api/menus.js
@@ -6,6 +6,37 @@ const menusRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+//Routes
+menusRouter.get('/', (req, res, next) => {
+  db.all('SELECT * FROM Menu', (err, menus) => {
+    if (err) {
+      next(err);
+    } else {
+      res.status(200).json({menus: menus});
+    }
+  });
+});
 
+menusRouter.post('/', (req, res, next) => {
+  const menu = req.body.menu;
+  if (!menu.title) {
+    return res.sendStatus(400);
+  }
+  db.run('INSERT INTO Menu (title) VALUES ($title)', {
+    $title: menu.title
+  }, function(err) {
+    if (err) {
+      next(err);
+    } else {
+      db.get(`SELECT * FROM Menu WHERE id = ${this.lastID}`, (err, menu) => {
+        if (err) {
+          next(err);
+        } else {
+          res.status(201).json({menu: menu});
+        }
+      });
+    }
+  });
+});
 
 module.exports = menusRouter;
